ac0519693c59ba10623c2c4f34d8a935a4c99d57
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2025-12-08 13:59:40 -0500

    feat: Added MQTT packet type constants for protocol implementation
    Defined packet type identifiers for CONNECT, CONNACK, PUBLISH, PUBACK, SUBSCRIBE, SUBACK, PINGREQ, PINGRESP, and DISCONNECT control packets as uint8 constants.

---
+++ internal/mqtt/models/packets/packets.go
@@ -0,0 +1,13 @@
+package packets
+
+var (
+	CONNECT    uint8 = 1
+	CONNACK    uint8 = 2
+	PUBLISH    uint8 = 3
+	PUBACK     uint8 = 4
+	SUBSCRIBE  uint8 = 8
+	SUBACK     uint8 = 9
+	PINGREQ    uint8 = 12
+	PINGRESP   uint8 = 13
+	DISCONNECT uint8 = 14
+)
