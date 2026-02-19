e2f4c7c3fb133017a117b378f896f80364692096
Angello de Jesus Bravo Mera <bravoangello31@gmail.com>
2026-02-19 10:44:07 -0500

    feat: Added PublishVarHeader struct and documented ConnectVarHeader fields
    Added PublishVarHeader with TopicName and PacketID fields for QoS > 0 scenarios. Added inline comments to ConnectVarHeader fields documenting protocol name, level, and connect flags components.

--- internal/mqtt/models/packets/variable_headers.go
+++ internal/mqtt/models/packets/variable_headers.go
@@ -1,8 +1,13 @@
 package packets
 
 type ConnectVarHeader struct {
-	ProtocolName  string
-	ProtocolLevel byte
-	ConnectFlags  byte
+	ProtocolName  string // MQTT
+	ProtocolLevel byte   // 3(?
+	ConnectFlags  byte   // clean session, will flag, will qos, will retain, password flag, username flag
 	KeepAlive     uint16
+}
+
+type PublishVarHeader struct {
+	TopicName string
+	PacketID  uint16 // Cuando QoS > 0
 }
